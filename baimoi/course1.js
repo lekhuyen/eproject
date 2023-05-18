77bad5d2181d0f5231909a7b8e08e40ae3c2a47f
lekhuyen <127919257+lekhuyen@users.noreply.github.com>
2023-05-18 10:04:47 +0700

    www
    wwww
    
    Co-Authored-By: PhamDuyHau <127919185+PhamDuyHau@users.noreply.github.com>
    Co-Authored-By: babayaga96 <127919127+babayaga96@users.noreply.github.com>
    Co-Authored-By: jortboy2 <127919074+jortboy2@users.noreply.github.com>

---
+++ baimoi/course1.js
@@ -0,0 +1,50 @@
+let thispage = 1;
+let limit = 6;
+let list = document.querySelectorAll('.row-1 .item-1')
+
+function loaditem() {
+    let begin = limit * (thispage - 1)
+    let endGet = limit*thispage - 1
+    list.forEach((item, key) => {
+        if(key>=begin && key<=endGet) {
+            item.style.display = 'block'
+        }else{
+            item.style.display = 'none'
+        }
+    })
+    lisPage()
+}
+loaditem()
+function lisPage() {
+    let count = Math.ceil(list.length / limit)
+    document.querySelector('.listpage').innerHTML = ''
+
+
+    if(thispage != 1){
+        let prev = document.createElement('li')
+        prev.innerText= "PREV"
+        prev.setAttribute('onclick', "changePage("+(thispage - 1)+")")
+        document.querySelector('.listpage').appendChild(prev)
+
+    }
+    for(i = 1; i<=count; i++) {
+        let newPage = document.createElement('li')
+        newPage.innerText = i
+        if(i == thispage) {
+            newPage.classList.add('active')
+        }
+        newPage.setAttribute('onclick', "changePage("+i+")")
+        document.querySelector('.listpage').appendChild(newPage)
+    }
+
+    if(thispage!=count){
+        let next = docment.createElement('li')
+        next.innerText = 'NEXT'
+        next.setAttribute('onclick', "changePage("+(thispage+1)+")")
+        document.querySelector('.listpage').appendChild(next)
+    }
+}
+function changePage(i) {
+    thispage = i
+    loaditem()
+}
--- baimoi/courses.js
+++ baimoi/courses.js
@@ -52,3 +52,66 @@ if(reviews) {
         reviews.classList.add('overview-1')
     })
 }
+
+
+// ........................................................
+let listelements = document.querySelectorAll(".link")
+listelements.forEach(listelement => {
+    listelement.addEventListener("click", ()=>{
+        if(listelement.classList.contains('active')){
+            listelement.classList.remove('active')
+        }else {
+            listelements.forEach(liste => {
+                liste.classList.remove('active')
+            })
+            listelement.classList.toggle('active')
+        }
+    })
+})
+
+
+const video1 = document.querySelector('.video-1-0')
+const video2 = document.querySelector('.video-1-2')
+
+if(video1) {
+    video1.addEventListener('click', function(){
+        document.querySelector('.modal-video-1').style.display= 'block'
+        document.querySelector('.none-icon').style.display='block'
+        document.querySelector('.none-icon-1').style.display='none'
+        document.querySelector('.modal-video-2').style.display= 'none'
+        video1.classList.add('video-background')
+        video2.classList.remove('video-background')
+    })
+}
+if(video2) {
+    video2.addEventListener('click', function(){
+        document.querySelector('.modal-video-1').style.display= 'none'
+        document.querySelector('.modal-video-2').style.display= 'block'
+        document.querySelector('.none-icon').style.display='none'
+        document.querySelector('.none-icon-1').style.display='block'
+        video1.classList.remove('video-background')
+        video2.classList.add('video-background')
+
+    })
+}
+
+
+const giaotrinhs = document.querySelectorAll('.giaotrinh')
+const modal = document.querySelector('.modal-1')
+const btnClose = document.querySelector('.modal-body-close')
+const modalbody = document.querySelector('.modal-body-111')
+for(const giaotrinh of giaotrinhs) {
+    giaotrinh.addEventListener('click', function(){
+        modal.classList.add('open')
+    })
+}
+btnClose.addEventListener('click', function(){
+    modal.classList.remove('open')
+})
+modal.addEventListener('click', function(){
+    modal.classList.remove('open')
+})
+modalbody.addEventListener('click', function(e){
+    e.stopPropagation()
+})
+
