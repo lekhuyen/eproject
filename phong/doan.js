5e798d81d2b6ebbfe9237b69e9b897c57aa12e18
jortboy2 <vonghung849@gmail.com>
2023-05-19 10:05:06 +0700

    aaaaaa
    Co-Authored-By: lekhuyen <127919257+lekhuyen@users.noreply.github.com>
    Co-Authored-By: babayaga96 <127919127+babayaga96@users.noreply.github.com>
    Co-Authored-By: PhamDuyHau <127919185+PhamDuyHau@users.noreply.github.com>

--- phong/doan.js
+++ phong/doan.js
@@ -21,7 +21,16 @@ var languageStrings = {
     "textpage2-2":"Explore Popular Courses",
     "course":"course",
     "view":"View All Categories",
-    "login":"Login"
+    "login":"Login",
+    "text5":"Categories Computer Engineering",
+    "text6":"Categories Gym",
+    "text7":"20% Offer Running - Join Today",
+    "text8":"Welcome to our special offer! We are pleased to announce that we are currently running a program with a remarkable offer just for you. With the 20% Offer Running  program, you have access to the latest knowledge and developments in the field of science. With a 20% discount, you will have the opportunity to save a significant part of your costs and at the same time increase your level of knowledge.",
+    "register":"Register Now",
+    "text9":"Frequently Asked Questions",
+    "text10":"What are the admissions requirements ?",
+    "text11":"The admissions requirements typically refer to the criteria that applicants need to meet in order to be considered for admission to a particular institution or program. These requirements can vary depending on the institution and program, but may include factors such as academic qualifications, standardized test scores, letters of recommendation, essays or personal statements, and extracurricular activities. It's important to research the specific admissions requirements for the institution or program you are interested in to ensure you meet all necessary criteria.",
+
     
   },
   "vn": {
@@ -38,7 +47,16 @@ var languageStrings = {
     "textpage2-2":"Khám phá các khóa học phổ biến",
     "course":"Khoá Học",
     "view":"Xem tất cả danh mục",
-    "login":"Đăng nhập"
+    "login":"Đăng nhập",
+    "text5":"Danh mục Kỹ thuật máy tính",
+    "text6":"Danh mục Phòng tập thể dục",
+    "text7":"Ưu đãi 20% đang chạy - Tham gia ngay hôm nay",
+    "text8":"Chào mừng đến với ưu đãi đặc biệt của chúng tôi! Chúng tôi vui mừng thông báo rằng chúng tôi hiện đang chạy một chương trình với ưu đãi đặc biệt chỉ dành cho bạn. Với chương trình Chạy ưu đãi 20%, bạn được tiếp cận với những kiến thức và sự phát triển mới nhất trong lĩnh vực khoa học. Với mức giảm giá 20%, bạn sẽ có cơ hội tiết kiệm một phần chi phí đáng kể, đồng thời nâng cao trình độ hiểu biết của mình.",
+    "register":"Đăng ký ngay",
+    "text9":"Các câu hỏi thường gặp",
+    "text10":"các yêu cầu nhập học là gì?",
+    "text11":"Các yêu cầu tuyển sinh thường đề cập đến các tiêu chí mà ứng viên cần phải đáp ứng để được xem xét nhập học vào một tổ chức hoặc chương trình cụ thể. Những yêu cầu này có thể khác nhau tùy thuộc vào trường và chương trình, nhưng có thể bao gồm các yếu tố như trình độ học vấn, điểm kiểm tra tiêu chuẩn, thư giới thiệu, bài tiểu luận hoặc tuyên bố cá nhân và các hoạt động ngoại khóa. Điều quan trọng là phải nghiên cứu các yêu cầu tuyển sinh cụ thể đối với trường hoặc chương trình mà bạn quan tâm để đảm bảo bạn đáp ứng tất cả các tiêu chí cần thiết.",
+
   },
   "hi": {
     "home": "होम",
@@ -54,7 +72,16 @@ var languageStrings = {
     "textpage2-2":"लोकप्रिय पाठ्यक्रम एक्सप्लोर करें",
     "course":"अवधि",
     "view":"सभी श्रेणियाँ देखें",
-    "login":"लॉग इन करें"
+    "login":"लॉग इन करें",
+    "text5":"श्रेणियाँ कंप्यूटर इंजीनियरिंग",
+    "text6":"श्रेणियाँ जिम",
+    "text7":"20% ऑफर चल रहा है - आज ही ज्वाइन करें",
+    "text8":"हमारी विशेष पेशकश में आपका स्वागत है! हमें यह घोषणा करते हुए प्रसन्नता हो रही है कि वर्तमान में हम केवल आपके लिए एक उल्लेखनीय प्रस्ताव के साथ एक कार्यक्रम चला रहे हैं। 20% ऑफर रनिंग कार्यक्रम के साथ, आपके पास विज्ञान के क्षेत्र में नवीनतम ज्ञान और विकास तक पहुंच है। 20% छूट के साथ, आपके पास अपनी लागत का एक महत्वपूर्ण हिस्सा बचाने का अवसर होगा और साथ ही साथ अपने ज्ञान के स्तर में वृद्धि होगी।",
+    "register":"अभी पंजीकरण करें",
+    "text9":"अक्सर पूछे जाने वाले प्रश्नों",
+    "text10":"प्रवेश आवश्यकताएँ क्या हैं?",
+    "text11":"प्रवेश आवश्यकताएँ आमतौर पर उन मानदंडों को संदर्भित करती हैं जिन्हें किसी विशेष संस्थान या कार्यक्रम में प्रवेश के लिए विचार करने के लिए आवेदकों को पूरा करने की आवश्यकता होती है। ये आवश्यकताएँ संस्था और कार्यक्रम के आधार पर भिन्न हो सकती हैं, लेकिन इसमें शैक्षणिक योग्यता, मानकीकृत परीक्षण स्कोर, अनुशंसा पत्र, निबंध या व्यक्तिगत विवरण और पाठ्येतर गतिविधियाँ जैसे कारक शामिल हो सकते हैं। यह सुनिश्चित करने के लिए कि आप सभी आवश्यक मानदंडों को पूरा करते हैं, यह सुनिश्चित करने के लिए कि आप जिस संस्थान या कार्यक्रम में रुचि रखते हैं, विशिष्ट प्रवेश आवश्यकताओं पर शोध करना महत्वपूर्ण है।",
+
   }
 };
 
